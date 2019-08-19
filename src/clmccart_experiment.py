3ea27234623a01410ed03cdd164e07fec433a301
Claire McCarthy <clmccart@microsoft.com>
2019-08-19 16:39:41 -0700

    Exported py file

---
+++ src/clmccart_experiment.py
@@ -0,0 +1,12 @@
+from sklearn import svm
+from sklearn import datasets
+import pickle
+
+clf = svm.SVC(gamma='scale')
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+clf.fit(X, y)
+
+pickle.dump(clf, open("model.pkl", "wb"))
