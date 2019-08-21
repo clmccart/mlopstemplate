dcf956b04eeccf273cf3771bac9d658b35d02aeb
Claire McCarthy <clmccart@microsoft.com>
2019-08-21 10:58:59 -0700

    First draft of data_ingestion.py

--- data_ingestion/data_ingestion.py
+++ data_ingestion/data_ingestion.py
@@ -3,16 +3,17 @@
 import os
 import string
 from azure.storage.blob import BlockBlobService
-import json
+import json    
+from io import StringIO
+import pandas as pd
 
 with open('secrets.json') as json_file:
     secrets = json.load(json_file)
 blob_secrets = secrets['blob']
 
 block_blob_service = BlockBlobService(account_name=blob_secrets['account_name'], account_key=blob_secrets['account_key'])
-container_name = 'data'
-local_path = './'
-local_file_name = 'temp_iris.csv'
+container_name = blob_secrets['container_name']
+
 
 
 # List the blobs in the container.
@@ -20,9 +21,8 @@ print("\nList blobs in the container")
 generator = block_blob_service.list_blobs(container_name)
 for blob in generator:
     print("\t Blob name: " + blob.name)
+    blobstring = block_blob_service.get_blob_to_text(container_name, blob.name).content
+    df = pd.read_csv(StringIO(blobstring))
+    print(df.head())
 
 
-# Download the blob(s).
-full_path_to_file2 = os.path.join(local_path)
-print("\nDownloading blob to " + full_path_to_file2)
-block_blob_service.get_blob_to_path(container_name, local_file_name, full_path_to_file2)
