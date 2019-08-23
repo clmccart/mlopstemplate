60bbe145ac3c77aa7bab80ed3b03166d18c49a3c
Claire McCarthy <clmccart@microsoft.com>
2019-08-23 11:28:18 -0700

    refactor tests

--- tests/test_data_preprocessing.py
+++ tests/test_data_preprocessing.py
@@ -9,19 +9,17 @@ class DataProcessorTests(unittest.TestCase):
     
     def test__preprocess_df_no_steps__returns_original_df(self):
         data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18]} 
-        original_df = _get_df(data)
-        dp = DataPreprocessor(original_df)
+        expected_df, dp = _setup(data, expected_data=None)
 
         returned_df = dp.donothing()
 
-        self.assertTrue(_check_equality(returned_df, original_df, dp))
+        self.assertTrue(_check_equality(returned_df, expected_df, dp))
 
     def test__drop_one_column__drops_one_column(self):
         data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18]} 
         expected_data = {'Age':[20, 21, 19, 18]} 
-        original_df = _get_df(data)
-        expected_df = _get_df(expected_data)
-        dp = DataPreprocessor(original_df)
+        expected_df, dp = _setup(data, expected_data)
+        
         returned_df = dp.drop_columns('Name')
         
         self.assertTrue(_check_equality(returned_df, expected_df, dp))
@@ -29,27 +27,25 @@ class DataProcessorTests(unittest.TestCase):
     def test__drop_list_of_columns__drops_columns(self):
         data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18], 'Type':[1, 0, 2, 1]} 
         expected_data = {'Type':[1, 0, 2, 1]} 
-        original_df = _get_df(data)
-        expected_df = _get_df(expected_data)
-        dp = DataPreprocessor(original_df)
+        expected_df, dp = _setup(data, expected_data)
+        
         returned_df = dp.drop_columns(['Name', 'Age'])
         
         self.assertTrue(_check_equality(returned_df, expected_df, dp))
 
     def test__remove_rows_based_on_col_val__value_not_present__no_change(self):
         data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18], 'Type':[1, 0, 2, 1]} 
-        original_df = _get_df(data)
-        dp = DataPreprocessor(original_df)
+        expected_df, dp = _setup(data, expected_data=None)
+        
         returned_df = dp.remove_rows_based_on_value(column_name='Name', bad_values=['Farley'])
         
-        self.assertTrue(_check_equality(returned_df, original_df, dp))
+        self.assertTrue(_check_equality(returned_df, expected_df, dp))
     
     def test__remove_rows_based_on_col_val__value_present__is_removed(self):
         data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18], 'Type':[1, 0, 2, 1]} 
         expected_data = {'Name':['Tom', 'krish', 'jack'], 'Age':[20, 19, 18], 'Type':[1, 2, 1]} 
-        original_df = _get_df(data)
-        expected_df = _get_df(expected_data)
-        dp = DataPreprocessor(original_df)
+        expected_df, dp = _setup(data, expected_data)
+        
         returned_df = dp.remove_rows_based_on_value(column_name='Name', bad_values=['nick'])
         
         self.assertTrue(_check_equality(returned_df, expected_df, dp))
@@ -57,12 +53,12 @@ class DataProcessorTests(unittest.TestCase):
     def test__remove_rows_based_on_col__multiple_values__is_removed(self):
         data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18], 'Type':[1, 0, 2, 1]} 
         expected_data = {'Name':['krish', 'jack'], 'Age':[19, 18], 'Type':[2, 1]} 
-        original_df = _get_df(data)
-        expected_df = _get_df(expected_data)
-        dp = DataPreprocessor(original_df)
+        expected_df, dp = _setup(data, expected_data)
+        
         returned_df = dp.remove_rows_based_on_value(column_name='Name', bad_values=['nick', 'Tom'])
 
         self.assertTrue(_check_equality(returned_df, expected_df, dp))
+
 
 def _check_equality(df1, df2, dp):
     return (df1.equals(df2) and df2.equals(dp.get_df()))
@@ -70,6 +66,15 @@ def _check_equality(df1, df2, dp):
 def _get_df(data):
     df = pd.DataFrame(data) 
     return df
+
+def _setup(data, expected_data):
+    original_df = _get_df(data)
+    if expected_data == None:
+        expected_df = original_df
+    else:
+        expected_df = _get_df(expected_data)
+    dp = DataPreprocessor(original_df)
+    return (expected_df, dp)
 
 if __name__ == '__main__':
     import xmlrunner
