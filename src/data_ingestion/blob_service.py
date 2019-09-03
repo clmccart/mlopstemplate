fe994b6d1c84e6a75c7656538edc3b22179bcb4e
Claire McCarthy <clmccart@microsoft.com>
2019-09-03 13:47:36 -0700

    blob implementation of iservice interface

--- src/data_ingestion/blob_service.py
+++ src/data_ingestion/blob_service.py
@@ -1,15 +1,27 @@
 from azure.storage.blob import BlockBlobService
 
 class BlobService(iService):
-    
+
     def get_df(self):
-        self.set_up_secrets()
         self.connect_to_blob()
         self.pull_file()
         return self.df
     
+    
     def connect_to_blob(self):
-        pass
+        self.block_blob_service = BlockBlobService(account_name=self.secrets.get_account_name(), 
+                                                    account_key=self.secrets.get_account_key())
+        self.container_name = self.secrets.get_container_name()
+        self.generator = self.block_blob_service.list_blobs(self.container_name)
+        print("Connected to Blob Successfully")
     
     def pull_file(self):
-        pass
+        for blob in self.generator:
+            if blob.name == self.desired_file:
+                print("\t Pulling " + blob.name + ". . .")
+                blobstring = self.block_blob_service.get_blob_to_text(self.container_name, blob.name).content
+                df = pd.read_csv(StringIO(blobstring))
+                print("Pull complete.")
+                self.df = df
+                return
+        raise FileNotFoundError('no file with the name {} was found'.format(self.desired_file))
--- src/data_ingestion/iservice.py
+++ src/data_ingestion/iservice.py
@@ -1,10 +1,18 @@
 from abc import ABC, abstractmethod
 
 class iService(ABC):
-    def __init__(self, desired_file=None, ):
+
+    def __init__(self, desired_file=None, service=None, secrets=None):
         if desired_file == None:
-            throw Exception("desired_file is None. You must pass a file for retrieval.")
-        pass
+            raise Exception("desired_file is None. You must specify a file for retrieval.")
+        if service == None:
+            raise Exception("service is None. You must specify what service you would like to pull the data from.")
+        if secrets == None:
+            raise Exception("secrets is None. You must specify the service you are using for secret handling.")
+        self.desired_file = desired_file
+        self.service = service
+        self.secrets = secrets
+        self.secrets.set_up_secrets()
     
     @abstractmethod
     def get_df(self):
