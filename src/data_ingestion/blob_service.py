745de41718046f4f88ce595b5212f1b7cd7cb2ac
Claire McCarthy <clmccart@microsoft.com>
2019-09-03 17:38:13 -0700

    refact blob service

--- src/data_ingestion/blob_service.py
+++ src/data_ingestion/blob_service.py
@@ -3,29 +3,39 @@ from src.data_ingestion.iservice import iService
 import pandas as pd
 from io import StringIO
 
-
 class BlobService(iService):
+    def __init__(self, desired_file, secret_provider):
+        super().__init__(desired_file, secret_provider)
+        self.container_name = self.secret_provider.secrets['container_name']    
 
     def get_df(self):
-        self.connect_to_blob()
-        self.pull_file()
-        return self.df
+        try:
+            self.connect_to_service()
+            self.locate_desired_file()
+            self.download_file_as_df()
+            return self.df
+        except:
+            raise Exception('Was not able to return a dataframe.')
     
-    def connect_to_blob(self):
-        secrets = self.secrets.get_secrets()
-        self.block_blob_service = BlockBlobService(account_name=secrets['account_name'], 
-                                                    account_key=secrets['account_key'])
-        self.container_name = secrets['container_name']
-        self.generator = self.block_blob_service.list_blobs(self.container_name)
-        print("Connected to Blob Successfully")
+    def connect_to_service(self):
+        try:
+            self.block_blob_service = BlockBlobService(account_name=self.secret_provider.secrets['account_name'], 
+                                                        account_key=self.secret_provider.secrets['account_key'])
+            self.container = self.block_blob_service.list_blobs(self.container_name)
+        except:
+            raise Exception('Unable to authenticate with provided secrets. Account name or key is incorrect')
     
-    def pull_file(self):
-        for blob in self.generator:
-            if blob.name == self.desired_file:
-                print("Pulling " + blob.name + ". . .")
-                blobstring = self.block_blob_service.get_blob_to_text(self.container_name, blob.name).content
-                df = pd.read_csv(StringIO(blobstring))
-                print("Pull complete.")
-                self.df = df
-                return
-        raise FileNotFoundError('no file with the name {} was found'.format(self.desired_file))
+    def locate_desired_file(self):
+        try:
+            container = self.block_blob_service.list_blobs(self.container_name)
+            for blob in container:
+                    if blob.name == self.desired_file:
+                        print("Pulling " + blob.name + ". . .")
+                        self.location = blob.name
+        except:
+            raise Exception("Unable to locate the desired file.")
+    
+    def download_file_as_df(self):
+        blobstring = self.block_blob_service.get_blob_to_text(self.container_name, self.location).content
+        self.df = pd.read_csv(StringIO(blobstring))
+ 
--- src/data_ingestion/iservice.py
+++ src/data_ingestion/iservice.py
@@ -2,15 +2,24 @@ from abc import ABC, abstractmethod
 
 class iService(ABC):
 
-    def __init__(self, desired_file=None, secrets=None):
+    def __init__(self, desired_file=None, secret_provider=None):
         if desired_file == None:
             raise Exception("desired_file is None. You must specify a file for retrieval.")
-        if secrets == None:
+        if secret_provider == None:
             raise Exception("secret_provider is None. You must specify the service you are using for secret handling.")
         self.desired_file = desired_file
-        self.secrets = secrets()
-        self.secrets.set_up_secrets()
+        self.secret_provider = secret_provider()
+        self.secret_provider.set_up_secrets()
     
     @abstractmethod
     def get_df(self):
         return
+
+    def connect_to_service(self):
+        pass
+    
+    def locate_desired_file(self):
+        pass
+    
+    def download_file_as_df(self):
+        pass
