633072af2fbd2548ce28a9e173f80265cd1b298d
Claire McCarthy <clmccart@microsoft.com>
2019-09-05 15:58:22 -0700

    factor out drop column tests

--- tests/tests_datapreprocessing/test_data_preprocessing.py
+++ tests/tests_datapreprocessing/test_data_preprocessing.py
@@ -18,31 +18,6 @@ class DataProcessorTests(unittest.TestCase):
 
         self.assertTrue(returned_df.equals(original_df))
 
-    def test__drop_one_column__drops_one_column(self):
-        data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18]} 
-        expected_data = {'Age':[20, 21, 19, 18]} 
-        expected_df = pd.DataFrame(expected_data)
-        original_df = pd.DataFrame(data)
-        steps = [DropColumnsStep('Name')]
-        preprocessor = DataPreprocessor(original_df, steps)
-
-        returned_df = preprocessor.preprocess()
-
-        self.assertTrue(returned_df.equals(expected_df))
-    
-    def test__drop_list_of_columns__drops_columns(self):
-        # Arrange
-        data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18], 'Type':[1, 0, 2, 1]} 
-        expected_data = {'Type':[1, 0, 2, 1]} 
-        original_df = pd.DataFrame(data)
-        expected_df = pd.DataFrame(expected_data)
-        steps = [DropColumnsStep(column_names=['Name', 'Age'])]
-        preprocessor = DataPreprocessor(original_df, steps)
-        # Act
-        returned_df = preprocessor.preprocess()
-        # Assert
-        self.assertTrue(returned_df.equals(expected_df))
-
     def test__multiple_steps_returns_as_expected(self):
         data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18], 'Type':[1, 0, 2, 1]} 
         expected_data = {'Type':[2]} 
