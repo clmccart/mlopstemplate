76d20773c4d813ab851b0fca00eaf91a8e234864
Claire McCarthy <clmccart@microsoft.com>
2019-09-06 09:13:02 -0700

    change to model code

--- src/model_building/clmccart_model_code.py
+++ src/model_building/clmccart_model_code.py
@@ -3,8 +3,10 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from src.data_preprocessing.data_preprocessor import DataPreprocessor
 from src.data_ingestion.data_ingestion import DataIngestor
-from src.data_ingestion.blob_service import BlobService
-from src.data_ingestion.secret_json_handler import SecretJSON
+from src.data_ingestion.services.blob_service import BlobService
+from src.data_ingestion.secretproviders.secret_json_handler import SecretJSON
+from src.data_preprocessing.steps.drop_columns import DropColumnsStep
+from src.data_preprocessing.steps.row_bc_col import RemoveRowBcColStep
 
 from sklearn import svm
 from sklearn import datasets
@@ -18,16 +20,17 @@ ingestor = DataIngestor(desired_file='iris.csv',
 df = ingestor.get_df()
 print(df.head())
 print("Data has been ingested")
-# dp = DataPreprocessor(df)
-# dp.drop_columns(['sepal_length'])
-# dp.remove_rows_based_on_value(column_name='sepal_width', 
-#                             bad_values=[3.5])
-# df = dp.get_df()
 
-# y = df['species']
-# X = df.drop(columns=['species'])
+steps = [DropColumnsStep('sepal_length'), 
+        RemoveRowBcColStep(column_name='sepal_width', bad_values=[3.5])]
 
-# clf.fit(X, y)
+dp = DataPreprocessor(df, steps)
+df = dp.preprocess()
+
+y = df['species']
+X = df.drop(columns=['species'])
+
+clf.fit(X, y)
  
-# pickle.dump(clf, open("model.pkl", "wb"))
-# print("pickle dumped") 
+pickle.dump(clf, open("model.pkl", "wb"))
+print("pickle dumped") 
