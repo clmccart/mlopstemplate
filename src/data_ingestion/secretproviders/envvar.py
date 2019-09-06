7cabf436f829a65cd584bfb1928f51915ffb995b
Claire McCarthy <clmccart@microsoft.com>
2019-09-06 09:59:29 -0700

    env variable secret provider

---
+++ src/data_ingestion/secretproviders/envvar.py
@@ -0,0 +1,25 @@
+from src.data_ingestion.secretproviders.isecret import iSecret
+import os
+
+class EnvVariableProvider(iSecret):
+
+    def __init__(self):
+        pass
+
+    def set_up_secrets(self):
+        self.retrieve_env_variables()
+        self.secrets = self.retrieve_secrets()   
+    
+    def retrieve_env_variables(self):
+        self.account_name = os.environ['ACCOUNTNAME']
+        self.account_key = os.environ['ACCOUNTKEY']
+        self.container_name = os.environ['CONTAINERNAME']
+    
+    def retrieve_secrets(self):
+       self.secrets = {
+           "account_name" : self.account_name,
+           "account_key" : self.account_key,
+           "container_name" : self.container_name
+       }
+       
+    
